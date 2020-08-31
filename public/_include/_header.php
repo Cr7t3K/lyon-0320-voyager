7b8f6c004e1b6f2041dd9aeb35268024ef7b0886
Cr7t3K <cvr.contact.pro@gmail.com>
2020-08-31 20:34:18 +0200

    test index

--- public/_include/_header.php
+++ public/_include/_header.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
     <!-- Font -->
